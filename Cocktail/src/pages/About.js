6274fe645b41983d6499dc5a0297c5f84c106b77
hirokr <hrr3645@gmail.com>
2024-10-26 00:55:32 +0600

    file added

---
+++ Cocktail/src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h2 className='section-title'>about us</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus sint praesentium necessitatibus harum quia beatae maxime adipisci doloribus, eligendi laudantium blanditiis facilis aspernatur ad voluptatibus placeat dolorem itaque possimus quis!
+      </p>
+    </section>
+  )
+}
+
+export default About
